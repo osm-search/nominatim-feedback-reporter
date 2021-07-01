e9b8e45a8d4f8926000fe871a1b2392405fcd94e
Darkshredder <srivastavayash58@gmail.com>
2021-07-01 16:02:30 +0530

    added local storage for bug descriptiiion page and connected it with thankyou page

--- frontend/src/pages/BugDescriptionPage.svelte
+++ frontend/src/pages/BugDescriptionPage.svelte
@@ -1,5 +1,18 @@
 <script>
-  import Header from '../components/Header.svelte';
+  import Header from "../components/Header.svelte";
+  import { getSetBugData } from "../lib/helpers.js";
+  import { refresh_page,error_store } from "../lib/stores.js";
+
+  export let feedbackDescription;
+
+  function handleClick() {
+    if (feedbackDescription) {
+      getSetBugData("final_bug_description", feedbackDescription);
+      refresh_page("thankyou");
+    } else {
+      error_store.set(`You need to add a feedback description`);
+    }
+  }
 </script>
 
 <Header />
@@ -8,16 +21,19 @@
     <h2>Feedback Description</h2>
   </div>
   <div class="container">
-    <form>
+    <form on:submit|preventDefault={handleClick}>
       <div class="form-group">
         <textarea
+          bind:value={feedbackDescription}
           class="form-control"
-          id="bugdescription"
+          id="feedbackdescription"
           rows="15"
           placeholder="Enter additional details"
         />
         <div class="float-end mt-3">
-          <button type="submit" class="btn btn-success">Submit Feedback Report </button>
+          <button type="submit" class="btn btn-success">
+            Submit Feedback Report
+          </button>
         </div>
       </div>
     </form>
